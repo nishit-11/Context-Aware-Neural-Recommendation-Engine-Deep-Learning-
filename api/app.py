56890cd76044d6d2da88e57fdc4e3b101abd1224
nagamalleswari <nagamalleswari601@gmail.com>
2026-07-08 18:23:03 +0530

    Add configuration and logging support

--- api/app.py
+++ api/app.py
@@ -1,21 +1,18 @@
 from fastapi import FastAPI
 from api.routes import router
+from api.config import settings
 
 app = FastAPI(
-    title="Context-Aware Recommendation API",
-    version="1.0.0"
+    title=settings.APP_NAME,
+    version=settings.VERSION
 )
 
 app.include_router(router)
 
 @app.get("/")
 def home():
-    return {
-        "message": "Recommendation API is Running"
-    }
+    return {"message": "Recommendation API is Running"}
 
 @app.get("/health")
 def health():
-    return {
-        "status": "healthy"
-    }
+    return {"status": "healthy"}
